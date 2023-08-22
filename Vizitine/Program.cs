00372e07a652621df81453bcab36b318811777fb
Lenovo <Lenovo@DESKTOP-5697MAH>
2023-08-22 18:50:25 +0300

    Add project files.

---
+++ Vizitine/Program.cs
@@ -0,0 +1,16 @@
+﻿String name = "Vardenis Pavardenis";
+int age = 40;
+String qvalification = "Software Developer";
+String mail = "johndoi@example.com";
+String phone = "+370 000 123456";
+
+
+Console.WriteLine("==========VIZITINE===========");
+Console.WriteLine();    
+Console.WriteLine("Vardas: " + name);
+Console.WriteLine("Amzius: " + age);
+Console.WriteLine("Pareigos: " + qvalification);
+Console.WriteLine("E. Pastas: " + mail);
+Console.WriteLine("Tel.: " +  phone);
+Console.WriteLine();
+Console.WriteLine("=============================");
